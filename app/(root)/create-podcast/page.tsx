b235dfa9a1484f679d88fe6ccd91c4dbef801647
erendoru <erendoru@hotmail.com>
2024-06-14 13:10:24 +0300

    layout and routing done

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreatePodcast() {
+  return <div>CreatePodcast</div>;
+}
+
+export default CreatePodcast;
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LeftSidebar() {
+  return (
+    <section className="left_sidebar">
+      <nav></nav>
+    </section>
+  );
+}
+
+export default LeftSidebar;
