03c4273bc1c6091d971cfd8ff4ce10c56927bf55
erendoru <erendoru@hotmail.com>
2024-06-15 17:32:56 +0300

    root and layout done, homepage card done

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function CreatePodcast() {
-  return <div>CreatePodcast</div>;
+  return (
+    <div>
+      <h1 className="text-20 font-bold text-white-1">Creating Podcast</h1>
+    </div>
+  );
 }
 
 export default CreatePodcast;
